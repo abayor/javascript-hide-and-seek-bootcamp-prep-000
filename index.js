e9ddb225dc45c5e77284e72ef2f4a84c72deba6c
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-10 04:39:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  selector = document.getElementById('app').querySelector('nested')
+// selector = document.querySelector('.nested')[0]
+  return selector;
+}
+
+ 
+function nestedTarget(){
+  
+}
+
+function incresaseRankBy(n){
+  
+}
+function deepestChild(){
+  
+}
